7fc07e5204fb08bb3ae06ba78779b16a59e5323d
alvenie <alvenie@bu.edu>
2024-11-12 22:57:35 -0500

    added par into lib

--- assigns/interp01/lib/lib.ml
+++ assigns/interp01/lib/lib.ml
@@ -1,6 +1,5 @@
 open Utils
 open My_parser
-open Par
 
 let rec subst (v: value) (x: string) (e: expr) : expr =
   match e with
@@ -85,6 +84,105 @@ let interp (s: string) : (value, error) result =
   | Some expr -> eval expr
   | None -> Error ParseFail
 
+
+  %{
+    open Utils
+    
+    let rec mk_app e es = 
+      match es with
+      | [] -> e
+      | x :: es -> mk_app (App (e, x)) es
+    %}
+    
+    %token IF
+    %token THEN
+    %token ELSE
+    %token LET
+    %token IN
+    %token FUN
+    %token ARROW
+    %token ADD
+    %token SUB
+    %token MUL
+    %token DIV
+    %token MOD 
+    %token LT 
+    %token LTE 
+    %token GT
+    %token GTE
+    %token EQ
+    %token NEQ
+    %token AND
+    %token OR
+    %token LPAREN
+    %token RPAREN
+    %token FALSE
+    %token TRUE
+    %token <int> NUM
+    %token <string> VAR
+    
+    %right OR
+    %right AND
+    %left LT LTE GT GTE EQ NEQ
+    %left ADD SUB
+    %left MUL DIV MOD
+    
+    %token EOF
+    
+    %start <Utils.prog> prog
+    
+    %%
+    
+    prog:
+      | e = expr; EOF { e }
+    
+    expr:
+      | IF; e1 = expr; THEN; e2 = expr; ELSE; e3 = expr
+        { If (e1, e2, e3) }
+      | LET; x = VAR; EQ; e1 = expr; IN; e2 = expr
+        { Let (x, e1, e2) }
+      | FUN; x = VAR; ARROW; e = expr
+        { Fun (x, e) }
+      | e = expr2
+        { e }
+    
+    %inline bop:
+      |ADD {Add}
+      |SUB {Sub}
+      |MUL {Mul}
+      |DIV {Div}
+      |MOD {Mod}
+      |LT {Lt}
+      |LTE {Lte}
+      |GT {Gt}
+      |GTE {Gte}
+      |EQ {Eq}
+      |NEQ {Neq}
+      |AND {And}
+      |OR {Or}
+      
+    expr2:
+      | e1 = expr2; op = bop; e2 = expr2
+        { Bop (op, e1, e2) }
+      | e = expr3; es = expr3*
+        { mk_app e es }
+    
+    expr3:
+      | LPAREN; RPAREN
+        { Unit }
+      | TRUE
+        { True }
+      | FALSE
+        { False }
+      | n = NUM
+        { Num n }
+      | x = VAR
+        { Var x }
+      | LPAREN; e = expr; RPAREN
+        { e }
+      
+    
+    
 let parse s =
   try Some (Par.prog Lex.read (Lexing.from_string s))
   with _ -> None
